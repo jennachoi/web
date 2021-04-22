35b9dfdaaa6a820d15905402b35dfb767c6532ae
admin <admin@YD02-08>
2021-04-22 17:57:15 +0900

    4/22 commit

--- webProj/src/common/EmpDAO.java
+++ webProj/src/common/EmpDAO.java
@@ -16,11 +16,12 @@ public class EmpDAO { // DB에서 데이터를 가져오는 작업
 
 	public Employee insertEmpBySeq(Employee emp) {
 		conn = DBCon.getConnect();
-		
+
 		Employee empl = new Employee();
 
 		String sql1 = "select employees_seq.nextval from dual";
-		String sql2 = "insert into emp_temp(employee_id, last_name, email, hire_date, job_id) values(?, ?, ?, ?, ?)";
+		String sql2 = "insert into emp_temp(employee_id, first_name, last_name, email, hire_date, job_id, salary, department_id) "
+				+ "values(?, ?, ?, ?, ?, ?, ?, 50)";
 		try {
 			int empId = 0;
 			stmt = conn.createStatement();
@@ -30,20 +31,24 @@ public class EmpDAO { // DB에서 데이터를 가져오는 작업
 			}
 			psmt = conn.prepareStatement(sql2);
 			psmt.setInt(1, empId);
-			psmt.setString(2, emp.getLastName());
-			psmt.setString(3, emp.getEmail());
-			psmt.setString(4, emp.getHireDate());
-			psmt.setString(5, emp.getJobId());
+			psmt.setString(2, emp.getFirstName());
+			psmt.setString(3, emp.getLastName());
+			psmt.setString(4, emp.getEmail());
+			psmt.setString(5, emp.getHireDate());
+			psmt.setString(6, emp.getJobId());
+			psmt.setInt(7, emp.getSalary());
 
 			int r = psmt.executeUpdate();
 			System.out.println(r + "건 입력됨.");
-			
-			//입력한 값을 반환해주기
+
+			// 입력한 값을 반환해주기
 			empl.setEmployeeId(empId);
+			empl.setFirstName(emp.getFirstName());
 			empl.setLastName(emp.getLastName());
 			empl.setEmail(emp.getEmail());
 			empl.setHireDate(emp.getHireDate());
 			empl.setJobId(emp.getJobId());
+			empl.setSalary(emp.getSalary());
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -53,14 +58,16 @@ public class EmpDAO { // DB에서 데이터를 가져오는 작업
 	}
 
 	public void insertEMP(Employee emp) {
-		String sql = "insert into emp_temp(employee_id, last_name, email, hire_date, job_id) values((select max(employee_id)+1 from emp_temp), ?, ?, ?, ?)";
+		String sql = "insert into emp_temp(employee_id, first_name, last_name, email, hire_date, job_id, salary) values((select max(employee_id)+1 from emp_temp), ?, ?, ?, ?, ?, ?)";
 		conn = DBCon.getConnect();
 		try {
 			psmt = conn.prepareStatement(sql);
-			psmt.setString(1, emp.getLastName());
-			psmt.setString(2, emp.getEmail());
-			psmt.setString(3, emp.getHireDate());
-			psmt.setString(4, emp.getJobId());
+			psmt.setString(1, emp.getFirstName());
+			psmt.setString(2, emp.getLastName());
+			psmt.setString(3, emp.getEmail());
+			psmt.setString(4, emp.getHireDate());
+			psmt.setString(5, emp.getJobId());
+			psmt.setInt(6, emp.getSalary());
 
 			int r = psmt.executeUpdate();
 			System.out.println(r + "건 입력됨.");
@@ -84,7 +91,7 @@ public class EmpDAO { // DB에서 데이터를 가져오는 작업
 
 	public List<Employee> getEmpByDept(String dept) {
 		// 사원정보를 가지고오는 처리
-		String sql = "select * from emp_temp where department_id = " + dept + "order by employee_id";
+		String sql = "select employee_id, first_name, last_name, email, to_char(hire_date, 'YYYY/MM/DD') as hire_date, job_id, salary from emp_temp where department_id = " + dept + "order by employee_id";
 		conn = DBCon.getConnect();
 		List<Employee> employees = new ArrayList<Employee>();
 
@@ -97,29 +104,28 @@ public class EmpDAO { // DB에서 데이터를 가져오는 작업
 				emp.setFirstName(rs.getString("first_name"));
 				emp.setLastName(rs.getString("Last_name"));
 				emp.setEmail(rs.getString("email"));
-				emp.setSalary(rs.getInt("Salary"));
-
+				emp.setHireDate(rs.getString("hire_date"));
+				emp.setJobId(rs.getString("job_id"));
+				emp.setSalary(rs.getInt("salary"));
+				
 				employees.add(emp);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if (rs != null) {
-				try {
+			try {
+				if (rs != null) {
 					rs.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			if (stmt != null) {
-				try {
+				} 
+				if (stmt != null) {
 					stmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
 				}
+			} 
+			catch (SQLException e) {
+			e.printStackTrace();
 			}
-		}
-		return employees;
+		} return employees;
+
 	}
 
 	public List<Employee> getEmpList() {
@@ -137,7 +143,9 @@ public class EmpDAO { // DB에서 데이터를 가져오는 작업
 				emp.setFirstName(rs.getString("first_name"));
 				emp.setLastName(rs.getString("Last_name"));
 				emp.setEmail(rs.getString("email"));
-				emp.setSalary(rs.getInt("Salary"));
+				emp.setHireDate(rs.getString("hire_date"));
+				emp.setJobId(rs.getString("job_id"));
+				emp.setSalary(rs.getInt("salary"));
 
 				employees.add(emp);
 			}
--- webProj/src/common/EmpServlet.java
+++ webProj/src/common/EmpServlet.java
@@ -17,27 +17,30 @@ public class EmpServlet extends HttpServlet {
 		PrintWriter out = resp.getWriter();
 		String dept = req.getParameter("dept");
 		EmpDAO dao = new EmpDAO();
-		
+
 		List<Employee> list = null;
-		
-		if(dept == null) {
+
+		if (dept == null) {
 			list = dao.getEmpList();
-		}else {
+		} else {
 			list = dao.getEmpByDept(dept);
 		}
-		
+
 		String jsonData = "[";
 		int cnt = 0;
-		
-		// [{"empId":"?", "fName":"?", "lName":"?" }, ... ] 와 같이 json 형식으로 
+
+		// [{"empId":"?", "fName":"?", "lName":"?" }, ... ] 와 같이 json 형식으로
 		for (Employee emp : list) {
-			jsonData += ("{\"empId\":\"" + emp.getEmployeeId() 
-			+ "\", \"fName\":\"" + emp.getFirstName()
-			+ "\", \"lName\":\"" + emp.getLastName()
-			+ "\", \"email\":\"" + emp.getEmail()
-			+ "\", \"salary\":\"" + emp.getSalary()
-					+ "\"}"); // "" 는 스트링타입을 선언, \는 escape해서 따옴표를 그대로 출력해줌 
-			if(++cnt == list.size()) {  // 마지막 데이터에는 콤마안찍기
+			jsonData += ("{\"empId\":\"" + emp.getEmployeeId() + "\", "
+					+ "\"fName\":\"" + emp.getFirstName() + "\", "
+					+ "\"lName\":\"" + emp.getLastName() + "\", "
+					+ "\"email\":\"" + emp.getEmail() + "\", "
+					+ "\"hiredate\":\"" + emp.getHireDate() + "\", "
+					+ "\"JobId\":\""+ emp.getJobId() + "\", "
+					+ "\"salary\":\""+ emp.getSalary() + "\"}"); 
+			
+			// "" 는 스트링타입을 선언, \는 escape해서 따옴표를 그대로 출력해줌
+			if (++cnt == list.size()) { // 마지막 데이터에는 콤마안찍기
 				continue;
 			}
 			jsonData += ",";
@@ -48,20 +51,34 @@ public class EmpServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String firstName = req.getParameter("first_name");
 		String lastName = req.getParameter("last_name");
 		String email = req.getParameter("email");
 		String hireDate = req.getParameter("hire_date");
 		String jobId = req.getParameter("job_id");
-		
+		String salary = req.getParameter("salary");
+		int sal = Integer.parseInt(salary);
+
 		Employee emp = new Employee();
+		emp.setFirstName(firstName);
 		emp.setLastName(lastName);
 		emp.setEmail(email);
 		emp.setHireDate(hireDate);
 		emp.setJobId(jobId);
+		emp.setSalary(sal);
+		
 
 		EmpDAO dao = new EmpDAO();
-		dao.insertEMP(emp);
-		
-		resp.getWriter().print("<h1>Success</h1>");
+		Employee empl = dao.insertEmpBySeq(emp);
+
+		// {"eid":"?","fName":"?" ... } 이런 형식으로 사용자가 입력한 값을 함께 다시 출력하도록 만들어줘야 함.
+		PrintWriter out = resp.getWriter();
+		out.print("{\"empId\":\"" + empl.getEmployeeId() + "\", "
+				+ "\"fName\":\"" + empl.getFirstName() + "\", "
+				+ "\"lName\":\"" + empl.getLastName() + "\", "
+				+ "\"email\":\"" + empl.getEmail() + "\", "
+				+ "\"hiredate\":\"" + empl.getHireDate() + "\", "
+				+ "\"JobId\":\""+ empl.getJobId() + "\", "
+				+ "\"salary\":\""+ empl.getSalary() + "\"}"); 
 	}
 }
